718a9dedb5dccb96a4d5420e9439ef1b0e6af54d
ginus4 <51823050+ginus4@users.noreply.github.com>
2020-01-28 14:39:34 +0100

    Create cactus.py

---
+++ cactus.py
@@ -0,0 +1,19 @@
+import pygame
+from loadimg import *
+scr_size = (width,height) = (600,150)
+
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self,speed=5,sizex=-1,sizey=-1):
+        pygame.sprite.Sprite.__init__(self,self.containers)
+        self.images,self.rect = load_sprite_sheet('cacti-small.png',3,1,sizex,sizey,-1)
+        self.rect.bottom = int(0.98*height)
+        self.rect.left = width + self.rect.width
+        self.image = self.images[random.randrange(0,3)]
+        self.movement = [-1*speed,0]
+
+    def update(self):
+        self.rect = self.rect.move(self.movement)
+
+        if self.rect.right < 0:
+            self.kill()
